606c98b0fa9642ebe86c328a01230dfdd7db33fc
adrianhdezruiz <44251@a.iesleopoldoqueipo.com>
2022-03-21 01:00:50 +0100

    Diseño página principal

---
+++ Código/vistas/principal.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../sass/custom.css">
+</head>
+<body>
+    <!-- Contenedor -->
+    <div class="container-fluid bg-success ">
+        <!--Cabecera-->
+        <header class="row bg-dark d-flex justify-content-center"> 
+            <img src="../../Imagenes/logo1.png" id="logo_header">
+        </header>
+
+        <!--Nav-->
+        <nav class="row  mb-3 w-75" style="margin: auto;">
+        <div class="col-3  p-3 mb-1  d-flex justify-content-center">
+                <select class="form-select" aria-label="Default select example" style="width:17rem; border: none;">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                </select>
+                <input type="submit" class="form-control ms-1 bg-dark text-success" style="width:6rem; border: none;" value="Buscar">
+            </div>
+
+            <div class="col-3 p-3 mb-1"></div>
+            <div class="col-3  p-3 mb-1 "></div>
+
+            <div class="col-3 p-3 mb-1  d-flex justify-content-center">
+                <input type="text" class="form-control me-5" style="width:24rem;" placeholder="Introduce un modelo...">
+
+            </div>
+        </nav>
+
+        <!--Main-->
+        <main class="row w-75" style="margin: auto;" >
+            <!--Fila 1 MAIN-->
+
+           <div class="row">
+                <div class="col-12 col-lg-3 col-md-6 col-sm-12 d-flex justify-content-center">
+                <div class="card text-center shadow p-3 mb-5 bg-body rounded" style="width: 24rem;">
+                    <img src="../../Imagenes/567213_00_1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        
+                        <h5 class="card-title text-dark mb-3">XIAOMI</h5>
+                        <a href="#" class="btn btn-dark w-100">Alquilar</a>
+                    </div>
+                </div>
+                </div>
+
+                <div class="col-12 col-lg-3 col-md-6 col-sm-12  d-flex justify-content-center">
+                <div class="card text-center shadow p-3 mb-5 bg-body rounded" style="width: 24rem;">
+                    <img src="../../Imagenes/567213_00_1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        
+                        <h5 class="card-title text-dark mb-3">XIAOMI</h5>
+                        <a href="#" class="btn btn-dark w-100">Alquilar</a>
+                    </div>
+                </div>
+                </div>
+
+                <div class="col-12 col-lg-3 col-md-6 col-sm-12  d-flex justify-content-center">
+                <div class="card text-center shadow p-3 mb-5 bg-body rounded" style="width: 24rem;">
+                    <img src="../../Imagenes/567213_00_1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        
+                        <h5 class="card-title text-dark mb-3">XIAOMI</h5>
+                        <a href="#" class="btn btn-dark w-100">Alquilar</a>
+                    </div>
+                </div>
+                </div>
+
+                <div class="col-12 col-lg-3 col-md-6 col-sm-12  d-flex justify-content-center">
+                <div class="card text-center shadow p-3 mb-5 bg-body rounded" style="width: 24rem;">
+                    <img src="../../Imagenes/567213_00_1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        
+                        <h5 class="card-title text-dark mb-3">XIAOMI</h5>
+                        <a href="#" class="btn btn-dark w-100">Alquilar</a>
+                    </div>
+                </div>
+                </div>
+               
+           </div>
+
+           <!--Fila 2 MAIN-->
+
+           <div class="row">
+
+           <div class="col-12 col-lg-3 col-md-6 col-sm-12  d-flex justify-content-center">
+                <div class="card text-center shadow p-3 mb-5 bg-body rounded" style="width: 24rem;">
+                    <img src="../../Imagenes/567213_00_1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        
+                        <h5 class="card-title text-dark mb-3">XIAOMI</h5>
+                        <a href="#" class="btn btn-dark w-100">Alquilar</a>
+                    </div>
+                </div>
+                </div>
+
+                <div class="col-12 col-lg-3 col-md-6 col-sm-12  d-flex justify-content-center">
+                <div class="card text-center shadow p-3 mb-5 bg-body rounded" style="width: 24rem;">
+                    <img src="../../Imagenes/567213_00_1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        
+                        <h5 class="card-title text-dark mb-3">XIAOMI</h5>
+                        <a href="#" class="btn btn-dark w-100">Alquilar</a>
+                    </div>
+                </div>
+                </div>
+
+                <div class="col-12 col-lg-3 col-md-6 col-sm-12  d-flex justify-content-center">
+                <div class="card text-center shadow p-3 mb-5 bg-body rounded" style="width: 24rem;">
+                    <img src="../../Imagenes/567213_00_1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        
+                        <h5 class="card-title text-dark mb-3">XIAOMI</h5>
+                        <a href="#" class="btn btn-dark w-100">Alquilar</a>
+                    </div>
+                </div>
+                </div>
+
+                <div class="col-12 col-lg-3 col-md-6 col-sm-12  d-flex justify-content-center">
+                <div class="card text-center shadow p-3 mb-5 bg-body rounded" style="width: 24rem;">
+                    <img src="../../Imagenes/567213_00_1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        
+                        <h5 class="card-title text-dark mb-3">XIAOMI</h5>
+                        <a href="#" class="btn btn-dark w-100">Alquilar</a>
+                    </div>
+                </div>
+                </div>
+
+          
+           </div>
+
+        </main>
+        <!-- Pie de página-->
+        <footer class="row bg-dark">
+            a
+        </footer>
+    </div>
+</body>
+</html>
