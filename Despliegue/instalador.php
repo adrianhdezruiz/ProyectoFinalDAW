8f8a4c3b383b934072f984ea37323c5a34b7a37f
adrianhdezruiz <44251@a.iesleopoldoqueipo.com>
2022-05-30 00:26:34 +0200

    Permisos subida imagenes

--- Despliegue/instalador.php
+++ Despliegue/instalador.php
@@ -2,14 +2,13 @@
 <?php
 
 try {
+    shell_exec("chown -R www-data:www-data /var/www/html/");
+    shell_exec("chmod -R /var/www/html/");
     shell_exec("/var/www/html/ProyectoFinalDAW/Despliegue/Scripts/script.sh");
     header("Location: ../Despliegue/aplicacion_instalada.php");
 } catch (\Throwable $th) {
     throw $th;
 }
-
-//shell_exec("chown -R www-data:www-data /var/www/html/");
-//shell_exec("chmod -R /var/www/html/");
 
 
 ?>
