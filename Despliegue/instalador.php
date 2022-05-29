f8c2fad1da8fd87925396806e977496f584f7166
adrianhdezruiz <44251@a.iesleopoldoqueipo.com>
2022-05-29 20:07:09 +0200

    Editado script despliegue

--- Despliegue/instalador.php
+++ Despliegue/instalador.php
@@ -1,15 +1,15 @@
 
 <?php
 
-if (!empty($_POST["instalar"])) {
-    if (shell_exec("/var/www/html/ProyectoFinalDAW/Despliegue/Script/script.sh")) {
-        echo "SCRIPT EJECUTADO CON EXITO";
-    } else {
-        echo "ERROR";
-    }
-
-    shell_exec("chown -R www-data:www-data /var/www/html/");
-    shell_exec("chmod -R /var/www/html/");
+try {
+    shell_exec("/var/www/html/ProyectoFinalDAW/Despliegue/Script/script.sh");
+    echo "SCRIPT EJECUTADO CON EXITO";
+} catch (\Throwable $th) {
+    throw $th;
 }
+
+shell_exec("chown -R www-data:www-data /var/www/html/");
+shell_exec("chmod -R /var/www/html/");
+
 
 ?>
