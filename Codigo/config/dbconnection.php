9de9218ae5094f6b65aca7e8f20f4c0978d5e5d3
adrianhdezruiz <44251@a.iesleopoldoqueipo.com>
2022-06-13 20:04:56 +0200

    Conexion base de datos

--- Codigo/config/dbconnection.php
+++ Codigo/config/dbconnection.php
@@ -1,10 +1,12 @@
 <?php
+session_start();
 $servername = "localhost";
 $username = "debianDB";
 $password = "debianDB";
+$bbdd = $_SESSION['bbddname'];
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=bdd", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=" . $bbdd, $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Connected successfully";
--- Despliegue/instalador.php
+++ Despliegue/instalador.php
@@ -1,7 +1,8 @@
 
 <?php
-
+session_start();
 $BBDD = $_POST['bbddname'];
+$_SESSION['bbddname'] = $BBDD;
 try {
     shell_exec("/var/www/html/ProyectoFinalDAW/Despliegue/Scripts/script.sh $BBDD");
     shell_exec("/var/www/html/ProyectoFinalDAW/Despliegue/Scripts/permisos.sh");
