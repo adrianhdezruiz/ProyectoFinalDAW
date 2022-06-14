59a17b0210e42227366c8c4fc6b23d8ad34abfc2
adrianhdezruiz <44251@a.iesleopoldoqueipo.com>
2022-06-14 04:48:38 +0200

    localhost

--- Despliegue/instalador.php
+++ Despliegue/instalador.php
@@ -18,7 +18,7 @@ $codigo = "
         \$bbdd = '$BBDD';
         
         try {
-            \$conn = new PDO('mysql:host=\$servername;dbname=' . \$bbdd, \$username, \$password);
+            \$conn = new PDO('mysql:host=localhost;dbname=' . \$bbdd, \$username, \$password);
             // set the PDO error mode to exception
             \$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             //echo 'Connected successfully';
